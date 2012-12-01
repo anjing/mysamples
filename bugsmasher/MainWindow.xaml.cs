3a1d566c5e6ae7358987a0f5386b593b9a11aabc
Shiwei Tu <Shiwei.Tu@bellmedia.ca>
2012-12-01 14:20:02 -0500

    finished implementation

--- bugsmasher/MainWindow.xaml.cs
+++ bugsmasher/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Timers;
-using System.Threading.Tasks;
+using System.Windows.Threading;
 
 
 namespace COMP212_Assignments07
@@ -22,21 +22,25 @@ namespace COMP212_Assignments07
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int score = 0;
         private int speed = 2400;
         private int aWidth = 0;
         private int aHeight = 0;
         private Random random;
 
         private Timer timer;
+
+
         public MainWindow()
         {
             InitializeComponent();
             random = new Random();
-            aWidth = (int)this.Width;
-            aHeight = (int)this.Height;
+            aWidth = (int)this.Width - 74;
+            aHeight = (int)this.Height - 95;
             timer = new Timer(speed);
             timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
             timer.Enabled = true;
+            btnRetScore_Click(this, null);
         }
 
         void timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -45,17 +49,20 @@ namespace COMP212_Assignments07
         }
 
         private void paintCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
+        {            
+            DetectHit();
         }
 
-        private void btnRestScore_Click(object sender, RoutedEventArgs e)
+        private void btnRetScore_Click(object sender, RoutedEventArgs e)
         {
-            this.lblScore.Content = 0;
+            score = 0;
+            this.lblScore.Content = score;
         }
 
         private void btnRestSpeed_Click(object sender, RoutedEventArgs e)
         {
-            speed = 0;
+            speed = 2400;
+            timer.Interval = speed;            
         }
 
         private void MoveBug()
@@ -64,14 +71,30 @@ namespace COMP212_Assignments07
             double y = random.Next(aHeight);
             if (x < 10)
                 x = 10;
+            if (x > aWidth - 50)
+                x = aWidth - 50;
             if (y < 10)
                 y = 10;
+            if (y > aHeight - 10)
+                y = aHeight;
+            Dispatcher.Invoke(new Action<double, double>(SetPos), x, y);
+        }
 
-            paintCanvas.Dispatcher.Invoke((delegate()
-            {
-                Canvas.SetTop(bug, y);
-                Canvas.SetLeft(bug, x);
-            });
+        private void SetPos(double x, double y)
+        {
+            Canvas.SetTop(bug, y);
+            Canvas.SetLeft(bug, x);
+        }
+
+        private void DetectHit()
+        {
+            score += 1;
+            this.lblScore.Content = score;
+            speed = speed - 200;
+            if (speed <= 200)
+                speed = 2400;
+            timer.Interval = speed;
+            MoveBug();
         }
     }
 }
